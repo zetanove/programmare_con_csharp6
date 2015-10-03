382c2f3b08d8928df333b7421fe51bf1912ae3d5
zetanove <antonio.pelleriti@gmail.com>
2015-10-03 18:19:32 +0200

    esempio Cap.2 Hello World

---
+++ Programmare con CSharp 6/Capitolo 2/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stampa Hello World
+            Console.WriteLine("Hello World");
+        }
+    }
+}
