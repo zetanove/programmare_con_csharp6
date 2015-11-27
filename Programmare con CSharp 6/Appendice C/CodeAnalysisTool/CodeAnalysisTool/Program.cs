84c24ebd5d75d0746edd74c1120434fc5badc2e1
zetanove <antonio.pelleriti@gmail.com>
2015-11-27 17:17:26 +0100

    esempi syntax transform

--- Programmare con CSharp 6/Appendice C/CodeAnalysisTool/CodeAnalysisTool/Program.cs
+++ Programmare con CSharp 6/Appendice C/CodeAnalysisTool/CodeAnalysisTool/Program.cs
@@ -25,13 +25,62 @@ namespace CodeAnalysisTool
                         {
                             Console.WriteLine(""Hello, World!"");
                         }
+                        
+                        public int Method1(string str)
+                        {
+                            return 0;
+                        }
                     }
                 }";
 
+        public void test()
+        {
+            Console.Write("Hello Matilda");
+        }
+
         static void Main(string[] args)
         {
+            TreeAnalysis();
+
+            SyntaxWalk();
+
+            SyntaxCreation();
+
+            SyntaxTransform();
+        }
+
+        private static void SyntaxWalk()
+        {
             SyntaxTree tree = CSharpSyntaxTree.ParseText(
-                code);
+                                       code);
+            MethodCollector collector = new MethodCollector();
+            collector.Visit(tree.GetRoot());
+            collector.methods.ForEach(m => Console.WriteLine(m));
+        }
+
+        private static void SyntaxCreation()
+        {
+           
+            var console = SyntaxFactory.IdentifierName("Console");
+            var writeline = SyntaxFactory.IdentifierName("WriteLine");
+            var memberaccess = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, console, writeline);
+
+            var argument = SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("Hello Matilda")));
+            var argumentList = SyntaxFactory.SeparatedList(new[] { argument });
+
+            var writeLineCall = SyntaxFactory.ExpressionStatement(
+                SyntaxFactory.InvocationExpression(memberaccess,
+                SyntaxFactory.ArgumentList(argumentList))
+            );
+
+
+            Console.WriteLine(writeLineCall.ToString());
+        }
+
+        private static void TreeAnalysis()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                            code);
 
             var root = (CompilationUnitSyntax)tree.GetRoot();
 
@@ -41,6 +90,7 @@ namespace CodeAnalysisTool
             }
 
             var firstMember = root.Members[0];
+            SyntaxKind kind = firstMember.Kind();
 
             var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
 
@@ -52,6 +102,20 @@ namespace CodeAnalysisTool
 
 
             Console.WriteLine($@"Il metodo {mainDeclaration.Identifier} ha il parametro {argsParameter.Identifier} di tipo {argsParameter.Type} e restituisce {mainDeclaration.ReturnType}");
+
+        }
+
+        private static void SyntaxTransform()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
+            CompilationUnitSyntax root = tree.GetRoot() as CompilationUnitSyntax;
+
+            var classDeclaration = root.DescendantNodes().OfType<ClassDeclarationSyntax>().Single();
+            var identifier = SyntaxFactory.Identifier("ProgramNew");
+            var newClassDeclaration = classDeclaration.WithIdentifier(identifier);
+
+            root = root.ReplaceNode(classDeclaration, newClassDeclaration);
+            Console.WriteLine(root.GetText());
         }
     }
 }
---
+++ Programmare con CSharp 6/Appendice C/CodeAnalysisTool/CodeAnalysisTool/WriteToWriteLineSyntaxRewriter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeAnalysisTool
+{
+    class WriteToWriteLineSyntaxRewriter: CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitCatchDeclaration(CatchDeclarationSyntax node)
+        {
+            return base.VisitCatchDeclaration(node);
+        }
+    }
+}
