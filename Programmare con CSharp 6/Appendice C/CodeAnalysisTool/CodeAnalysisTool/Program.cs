4f19f8c7a2cc33c987893f94bbf893ae3cad8f35
zetanove <antonio.pelleriti@gmail.com>
2015-11-28 22:50:43 +0100

    Emit API

--- Programmare con CSharp 6/Appendice C/CodeAnalysisTool/CodeAnalysisTool/Program.cs
+++ Programmare con CSharp 6/Appendice C/CodeAnalysisTool/CodeAnalysisTool/Program.cs
@@ -7,6 +7,11 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.Formatting;
+using System.IO;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
 
 namespace CodeAnalysisTool
 {
@@ -14,7 +19,7 @@ namespace CodeAnalysisTool
     {
         private const string code = @"using System;
                 using System.Collections;
-                using System.Linq;
+                using System.IO;
                 using System.Text;
 
                 namespace HelloWorld
@@ -33,6 +38,25 @@ namespace CodeAnalysisTool
                     }
                 }";
 
+        private const string codeTryCatch = @"using System;
+
+                namespace HelloWorld
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            try
+                            {
+                                Console.WriteLine(""Hello, World!"");
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                }";
+
         public void test()
         {
             Console.Write("Hello Matilda");
@@ -42,20 +66,81 @@ namespace CodeAnalysisTool
         {
             TreeAnalysis();
 
+            SearchInTree();
+
             SyntaxWalk();
 
             SyntaxCreation();
 
             SyntaxTransform();
+
+            SyntaxReplacer();
+
+            CompileCode();
+        }
+
+        private static void CompileCode()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
+            SyntaxTree[] sourceTrees = { tree };
+
+            MetadataReference mscorlib = 
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            MetadataReference codeAnalysis =
+                    MetadataReference.CreateFromFile(typeof(SyntaxTree).Assembly.Location);
+            MetadataReference csharpCodeAnalysis =
+                    MetadataReference.CreateFromFile(typeof(CSharpSyntaxTree).Assembly.Location);
+
+            MetadataReference[] references = { mscorlib, codeAnalysis, csharpCodeAnalysis };
+
+            CSharpCompilation compilation = CSharpCompilation.Create("RoslynCS",
+                                            sourceTrees,
+                                            references,
+                                            new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    string ext = (compilation.Options.OutputKind == OutputKind.ConsoleApplication || compilation.Options.OutputKind == OutputKind.WindowsApplication) ?
+                        ".exe" : ".dll";
+                    using (var file = File.Create(compilation.AssemblyName+ext))
+                    {
+                        ms.WriteTo(file);
+                    }
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(ms.ToArray());
+                }
+            }
         }
 
         private static void SyntaxWalk()
         {
-            SyntaxTree tree = CSharpSyntaxTree.ParseText(
-                                       code);
+            SyntaxTree tree = CSharpSyntaxTree.ParseText( code);
             MethodCollector collector = new MethodCollector();
             collector.Visit(tree.GetRoot());
             collector.methods.ForEach(m => Console.WriteLine(m));
+        }
+
+        private static void SyntaxReplacer()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(codeTryCatch);
+            CatchSyntaxRewriter rewriter = new CatchSyntaxRewriter();
+            var newRoot=rewriter.Visit(tree.GetRoot());
+            Console.WriteLine(newRoot.ToFullString());
         }
 
         private static void SyntaxCreation()
@@ -105,6 +190,20 @@ namespace CodeAnalysisTool
 
         }
 
+        private static void SearchInTree()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                            code);
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            var methods=root.DescendantNodes().OfType<MethodDeclarationSyntax>().Where(m => m.Identifier.Text.StartsWith("Method"));
+            
+            foreach(var method in methods)
+                Console.WriteLine($"metodo {method.ToString()}");
+
+        }
+
         private static void SyntaxTransform()
         {
             SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
@@ -115,6 +214,29 @@ namespace CodeAnalysisTool
             var newClassDeclaration = classDeclaration.WithIdentifier(identifier);
 
             root = root.ReplaceNode(classDeclaration, newClassDeclaration);
+
+
+            var console = SyntaxFactory.IdentifierName("Console");
+            var writeline = SyntaxFactory.IdentifierName("WriteLine");
+            var memberaccess = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, console, writeline);
+
+            var argument = SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("Hello Matilda")));
+            var argumentList = SyntaxFactory.SeparatedList(new[] { argument });
+
+            var writeLineCall = SyntaxFactory.ExpressionStatement(
+                SyntaxFactory.InvocationExpression(memberaccess,
+                SyntaxFactory.ArgumentList(argumentList))
+            );
+
+           
+            classDeclaration = root.DescendantNodes().OfType<ClassDeclarationSyntax>().Single();
+            var last = classDeclaration.ChildNodes().Last();
+            newClassDeclaration = classDeclaration.RemoveNode(last, SyntaxRemoveOptions.KeepEndOfLine);
+            root = root.ReplaceNode(classDeclaration, newClassDeclaration);
+
+            var lastStatement= root.DescendantNodes().OfType<StatementSyntax>().Last();            
+            root = root.InsertNodesAfter(lastStatement, new SyntaxNode[] { writeLineCall });
+            
             Console.WriteLine(root.GetText());
         }
     }
