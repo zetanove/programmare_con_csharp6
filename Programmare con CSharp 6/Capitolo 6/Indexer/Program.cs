97180e1fac477f64f07daffe5e88c1d662cd8699
zetanove <antonio.pelleriti@gmail.com>
2016-07-17 23:29:08 +0200

    capitolo 6

---
+++ Programmare con CSharp 6/Capitolo 6/Indexer/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Indexer
+{
+    public class Smartphone
+    {
+        public class TelephoneNumber
+        {
+            public string Nome {get;set;}
+            public int Number { get; set; }
+        }
+
+        private TelephoneNumber[] numeri;
+
+        public TelephoneNumber this[int index]
+        {
+            get
+            {
+                return numeri[index];
+            }
+            set
+            {
+                numeri[index] = value;
+            }
+        }
+
+        public TelephoneNumber this[string nome]
+        {
+            get
+            {
+                foreach (TelephoneNumber number in numeri)
+                {
+                    if (number.Nome == nome)
+                        return number;
+                }
+                return null;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone sp = new Smartphone();
+            Smartphone.TelephoneNumber primo = sp[0];
+        }
+    }
+}
